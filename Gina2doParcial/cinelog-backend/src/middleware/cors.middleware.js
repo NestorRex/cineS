c12f792570cd78f4fef7386590500f1111631271
Romain Díaz de León <105795204+Nesscafebaby@users.noreply.github.com>
2026-04-18 16:40:27 -0600

    Delete Gina2doParcial directory

--- Gina2doParcial/cinelog-backend/src/middleware/cors.middleware.js
+++
@@ -1,9 +0,0 @@
-import cors from 'cors';
-
-const corsMiddleware = cors({
-  origin: process.env.FRONTEND_URL || '*',
-  methods: ['GET', 'POST', 'DELETE', 'PUT'],
-  allowedHeaders: ['Content-Type'],
-});
-
-export default corsMiddleware;
--- Gina2doParcial/cinelog-frontend/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://TU-PROYECTO.onrender.com/api'  // Cambiar por tu URL real
-};
